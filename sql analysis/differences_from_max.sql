41f5025fb2b74b1b2fdfb73e60228590ca2c3809
Rohde Fischer <rohdef@rohdef.dk>
2015-05-20 21:44:33 +0200

    Differences

---
+++ sql analysis/differences_from_max.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW differences_from_max AS (
+SELECT a.start_time, a.avg_effect, o.max, (o.max-a.avg_effect) AS difference, ROUND(((o.max-a.avg_effect)/o.max), 4) AS percent FROM avg_effects a
+LEFT JOIN optimal_effects o ON EXTRACT(hours FROM a.start_time)=o.hours AND EXTRACT(minutes FROM a.start_time)=o.minutes
+);
